7a569a217d751a992dd136bd4862ee06ffee731f
Bagus Aji Santoso <baguzzzaji@gmail.com>
2016-03-27 09:49:38 +0700

    MainActivityFragment

--- app/src/main/java/net/sebariskode/moviedb/MainActivityFragment.java
+++ app/src/main/java/net/sebariskode/moviedb/MainActivityFragment.java
@@ -1,9 +1,44 @@
 package net.sebariskode.moviedb;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.GridView;
+
+import java.util.Arrays;
 
 /**
  * Created by baguzzzaji on 3/27/16.
  */
 public class MainActivityFragment extends Fragment {
+
+    private MovieAdapter movieAdapter;
+
+    Movie[] movies = {
+            new Movie("Deadpool", "Yeah", 8.9, "012816", "poster.png"),
+            new Movie("Superman", "Son of Krypton", 9.0, "022416", "poster.png"),
+            new Movie("Batman", "King of Gotham", 9.5, "032416", "poster.png"),
+            new Movie("Avenger", "So so", 8.0, "031616", "poster.png")
+    };
+
+    public MainActivityFragment() {
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        movieAdapter = new MovieAdapter(getActivity(), Arrays.asList(movies));
+
+        GridView gridView = (GridView) rootView.findViewById(R.id.movies_grid);
+        gridView.setAdapter(movieAdapter);
+
+        return rootView;
+    }
 }
