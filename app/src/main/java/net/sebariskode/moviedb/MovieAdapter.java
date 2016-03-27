9f7a2456925c30c739c06313c17fa0852a122883
Bagus Aji Santoso <baguzzzaji@gmail.com>
2016-03-27 10:05:32 +0700

    Use Picasso to show poster image on gridview

--- app/src/main/java/net/sebariskode/moviedb/MovieAdapter.java
+++ app/src/main/java/net/sebariskode/moviedb/MovieAdapter.java
@@ -5,6 +5,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
@@ -26,6 +29,9 @@ public class MovieAdapter extends ArrayAdapter<Movie> {
         if (convertView == null) {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.movie_item, parent, false);
         }
+
+        ImageView imageView = (ImageView) convertView.findViewById(R.id.movie_poster);
+        Picasso.with(getContext()).load(movie.moviePoster).into(imageView);
 
         return convertView;
     }
