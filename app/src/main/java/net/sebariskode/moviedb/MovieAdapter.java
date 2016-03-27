d40fd973ae29ea94d9611c6f3ac52000d4042d84
Bagus Aji Santoso <baguzzzaji@gmail.com>
2016-03-27 07:53:02 +0700

    Add ArrayAdapter for Movie

---
+++ app/src/main/java/net/sebariskode/moviedb/MovieAdapter.java
@@ -0,0 +1,32 @@
+package net.sebariskode.moviedb;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import java.util.List;
+
+/**
+ * Created by baguzzzaji on 3/27/16.
+ */
+public class MovieAdapter extends ArrayAdapter<Movie> {
+    private static final String LOG_TAG = MovieAdapter.class.getSimpleName();
+
+
+    public MovieAdapter(Activity context, List<Movie> movies) {
+        super(context, 0, movies);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        Movie movie = getItem(position);
+
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.movie_item, parent, false);
+        }
+
+        return convertView;
+    }
+}
