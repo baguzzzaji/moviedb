9db8613ce5785155006a469f3ce5a7886e4369dc
Bagus Aji Santoso <baguzzzaji@gmail.com>
2016-03-27 07:44:28 +0700

    Add MainActivityFragment

---
+++ app/src/main/java/net/sebariskode/moviedb/MainActivityFragment.java
@@ -0,0 +1,9 @@
+package net.sebariskode.moviedb;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by baguzzzaji on 3/27/16.
+ */
+public class MainActivityFragment extends Fragment {
+}
